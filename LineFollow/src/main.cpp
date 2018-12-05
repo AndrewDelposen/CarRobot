0953d2ca9dfe6467ec82f563815843ea4dcbc292
Andrew Delposen <ajd7500@cs.rit.edu>
2018-12-05 11:32:14 -0500

    Outlined line following code

--- LineFollow/src/main.cpp
+++ LineFollow/src/main.cpp
@@ -2,6 +2,11 @@
 
 //GLOBAL VARIABLES
 int BASE_SPEED = 125; //125 = "half power"
+int SMALL_SLOW_SPEED_CHANGE = 113; //10% decrease for sensor slightly hitting black
+int SMALL_FAST_SPEED_CHANGE = 134; //10% increase
+int MED_SLOW_SPEED_CHANGE = 94; //25% decrease
+int MED_FAST_SPEED_CHANGE = 156; //25% increase
+
 
 //////Motors definitions//////////
 #define Motors_nD2 4 // Enable Pin
@@ -32,10 +37,10 @@ void setup(){
 
 	//Start moving left motor
     //analogWrite(MotorL_PWM, BASE_SPEED); //"Half power"
-    //digitalWrite(MotorL_DIR, HIGH); //HIGH = 1, meaning foward
+    //digitalWrite(MotorL_DIR, HIGH); //HIGH = 1, meaning forward
     //Start moving right motor
     //analogWrite(MotorR_PWM, BASE_SPEED); //"Half power"
-    //digitalWrite(MotorR_DIR, HIGH); //HIGH = 1, meaning foward
+    //digitalWrite(MotorR_DIR, HIGH); //HIGH = 1, meaning forward
 
 // cnt 
     //ext
@@ -67,6 +72,83 @@ void loop(){
     delay(1000);
 }
 
+/*
+	leftSensorVal = readQD(LEFT_SENSOR_PIN);
+	Serial.print("Left sensor value: "); Serial.println(leftSensorVal);
+	
+	if(leftSensorVal <= 799){
+		continue; //don't waste processing time w/ other checks
+	}
+	else if(leftSensorVal >= 800 && leftSensorVal <= 850){
+		//Slow left wheel by SMALL amount
+		analogWrite(MotorL_PWM, SMALL_SLOW_SPEED_CHANGE);
+		//Speed right wheel by SMALL amount
+		analogWrite(MotorR_PWM, SMALL_FAST_SPEED_CHANGE);
+	}
+	else if(leftSensorVal >= 851 && leftSensorVal <= 1200){
+		//Slow left wheel by MEDIUM amount
+		analogWrite(MotorL_PWM, SMALL_SLOW_SPEED_CHANGE);
+		//Speed right wheel by MEDIUM amount
+		analogWrite(MotorR_PWM, SMALL_FAST_SPEED_CHANGE);
+	}
+	else if (leftSensorVal >= 1300){ //basically on black
+		//Active stop BOTH wheels
+		analogWrite(MotorL_PWM, 0);
+		analogWrite(MotorR_PWM, 0);
+
+		//Set left wheel to backwards direction
+		digitalWrite(MotorL_DIR, LOW); //HIGH = 1, meaning forward
+
+		//MOVE BOTH wheels to base speeds
+		analogWrite(MotorL_PWM, BASE_SPEED);
+		analogWrite(MotorR_PWM, BASE_SPEED);
+
+		//MOVE until left sensor is OFF black line
+		while(leftSensorVal > 850){
+			leftSensorVal = readQD(LEFT_SENSOR_PIN);
+		}
+		
+		//Active stop both wheels
+		analogWrite(MotorL_PWM, 0);
+		analogWrite(MotorR_PWM, 0);
+		
+		//Set left wheel to forward direction
+		digitalWrite(MotorL_DIR, HIGH); //HIGH = 1, meaning forward
+		
+		//MOVE BOTH wheels to base speeds
+		analogWrite(MotorL_PWM, BASE_SPEED);
+		analogWrite(MotorR_PWM, BASE_SPEED);
+		
+		//IGNORE RIGHT WHEEL SENSOR check? 
+	}
+	
+	rightSensorVal = readQD(RIGHT_SENSOR_PIN);
+	Serial.print("Right sensor value: "); Serial.println(rightSensorVal);
+
+	if(rightSensorVal <= 799){
+		continue; //don't waste processing time w/ other checks
+	}
+	else if(rightSensorVal >= 800 && rightSensorVal <= 850){
+		right wheel slow by SMALL %
+		left wheel fast by SMALL %
+	}
+	else if(rightSensorVal >= 851 && rightSensorVal <= 1200){
+		right wheel slow by MEDIUM %
+		left wheel fast by MEDIUM %
+	}
+	else if (rightSensorVal >= 1300){ //basically on black
+		active stop
+		right wheel set to backwards
+		right wheel forward
+		left wheel forward
+		while(rightSensorVal > 850){
+			rightSensorVal = readQD(LEFT_SENSOR_PIN);
+		}
+		active stop wheels
+		set right wheel to forwards 
+	}
+	
+*/
 //IMPORTANT IR REFLECTION DATA
 // robot @ line    | L SENSOR | R SENSOR
 // perfect between | 650-700      | 800
